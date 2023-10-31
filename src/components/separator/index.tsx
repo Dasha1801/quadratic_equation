0bc0962ca3c7b8ac009b2ff538b79f29b9a306a2
Darya Belskaya <dariya.b@itfox-web.com>
2023-10-31 22:19:14 +0300

    feat: delete folder public

--- src/components/separator/index.tsx
+++ src/components/separator/index.tsx
@@ -1,9 +1,10 @@
+import arrow from '../../assets/icons/arrow.svg'
 import './style.scss'
 
 const Separator = () => {
   return (
     <div className="separator">
-      <img alt="" src="public/assets/icons/arrow.svg" />
+      <img alt="" src={arrow} />
     </div>
   )
 }
