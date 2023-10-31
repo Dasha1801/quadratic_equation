498c2f91bf73631860cc1537997ccce76014b690
Darya Belskaya <dariya.b@itfox-web.com>
2023-10-31 15:15:46 +0300

    feat: add enter user component

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,10 +1,44 @@
+import { useState, useMemo } from 'react'
 import EnterUserData from '../enterUserData'
 import './style.scss'
+
+export interface IState {
+  a: string
+  b: string
+  c: string
+}
+
+const InitState = {
+  a: '',
+  b: '',
+  c: '',
+}
+
 const App = () => {
+  const [state, setState] = useState<IState>(InitState)
+
+  const onChange = (key: keyof typeof state, val: string) => {
+    setState({ ...state, [key]: val.replace(/[^0-9]/g, '') })
+  }
+
+  const resetData = () => setState(InitState)
+
+  const isDisabled = useMemo(
+    () => Object.values(state).some((val) => val === ''),
+    [state]
+  )
   return (
     <div className="wrapperApp">
       <h1>Решение квадратного уравнения</h1>
-      <EnterUserData />
+      <EnterUserData data={state} onChange={onChange} />
+      <div className="actions">
+        <div className={`btn ${isDisabled ? 'notValid' : ''}`}>
+          Рассчитать корни
+        </div>
+        <div className="btn red" onClick={resetData}>
+          Очистить
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/enterUserData/index.tsx
+++ src/components/enterUserData/index.tsx
@@ -1,11 +1,37 @@
+import { IState } from '../app'
 import './style.scss'
 
-const EnterUserData = () => {
+interface IPropsData {
+  data: IState
+  onChange: (key: keyof IState, val: string) => void
+}
+
+const EnterUserData = ({ data: { a, b, c }, onChange }: IPropsData) => {
   return (
     <div className="wrapperData">
-      <div className="text">
-        Введите данные для расчета корней уавнения. Данные могут содержать
-        цифры, знаки: &quot;-&quot;, &quot;.&quot;
+      <div className="title">
+        ax<sup>2</sup> + bx + c = 0
+      </div>
+      <div className="text">Введите данные для расчета корней уравнения.</div>
+      <div className="expression">
+        <input
+          value={a}
+          type="number"
+          onChange={(e) => onChange('a', e.target.value)}
+        />{' '}
+        x <sup>2</sup> + {'  '}
+        <input
+          value={b}
+          type="number"
+          onChange={(e) => onChange('b', e.target.value)}
+        />{' '}
+        x +{' '}
+        <input
+          value={c}
+          type="number"
+          onChange={(e) => onChange('c', e.target.value)}
+        />{' '}
+        = 0
       </div>
     </div>
   )
